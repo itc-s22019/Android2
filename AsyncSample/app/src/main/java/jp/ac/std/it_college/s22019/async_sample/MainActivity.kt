207282195b3b200383be874a335e37159acf14ed
itc-s22019 <s22019@std.it-college.ac.jp>
2023-10-03 11:04:03 +0900

    initial commit for chap11

---
+++ AsyncSample/app/src/main/java/jp/ac/std/it_college/s22019/async_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.std.it_college.s22019.async_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
